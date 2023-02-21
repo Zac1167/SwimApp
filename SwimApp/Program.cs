2e25f85361129594ab25f2e1e78f2fc3912fe025
Zachary Dakins <zacdaki@chbc.school.nz>
2023-02-21 13:30:43 +1300

    21/02/23

--- SwimApp/Program.cs
+++ SwimApp/Program.cs
@@ -10,6 +10,8 @@ namespace SwimApp
         static List<string> teamB = new List<string>(); 
         static List<string> teamReserves = new List<string>();
 
+        static float fastestTime = 99999f;
+        static string topSwimmer = "";
         static void OneSwimmer()
         {
             Console.WriteLine("Enter the swimmer's name\n");
@@ -27,32 +29,48 @@ namespace SwimApp
                 minutes = randomNumber.Next(1, 4);
                 seconds = randomNumber.Next(0, 59);
                 totalTime = (minutes * 60) + seconds;
-                Console.WriteLine($"Swimmer Time {i + 1}: {minutes} min, {seconds} sec\nTotal Time in Seconds: {totalTime}sec\n");
+                Console.WriteLine($"Swimmer Time {i + 1}: {minutes} min, {seconds} sec\tTotal Time in Seconds: {totalTime}sec\n");
                 //Next line is = to: sumTotalTime = sumTotalTime + totalTime
                 sumTotalTime += totalTime;
             }
 
             float avgTime = (float)sumTotalTime / 4;
+            string allocatedTeam = "Reserve";
             
             //Assign the swimmer to a team
             if(avgTime <= 160)
             {
                 teamA.Add(swimmerName);
+                allocatedTeam = "A";
             }
             else if(avgTime <= 210)
             {
                 teamB.Add(swimmerName);
+                allocatedTeam = "B";
             }
             else
             {
                 teamReserves.Add(swimmerName);
             }
-            Console.WriteLine($"Avg time = {avgTime}");
+            Console.WriteLine($"Avg time = {avgTime}\n");
+            if (avgTime <= fastestTime)
+            {
+                fastestTime = avgTime;
+                topSwimmer = 
+            }
+            Console.WriteLine($"{swimmerName} = Team {allocatedTeam}");
         }
             
         static void Main(string[] args)
         {
-            OneSwimmer();
+            string flag = "Stop";
+            while (!flag.Equals(""))
+            {
+                OneSwimmer();
+                Console.WriteLine("\nPress 'Enter' to add another swimmer.\nOr type 'Stop' to end.");
+                flag = Console.ReadLine();
+            }
+            Console.WriteLine($"The fastest swimmer was {topSwimmer} with an average time of {fastestTime}!");
         }
     }
 }
